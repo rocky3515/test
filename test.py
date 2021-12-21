e8e93a23ed07533ee8905fab12db0f7084c8e2a2
rocky3515 <173823778@qq.com>
2021-12-21 16:00:55 +0800

    c

---
+++ test.py
@@ -0,0 +1,8 @@
+'''
+Author: your name
+Date: 2021-12-21 16:00:34
+LastEditTime: 2021-12-21 16:00:35
+LastEditors: Please set LastEditors
+Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+FilePath: \git_test\test.py
+'''
